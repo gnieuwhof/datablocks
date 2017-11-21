941497279d17db7cdc668e58d68f737cb787a1ee
Guido Nieuwhof <guido@Guidos-MacBook-Pro.local>
2017-11-21 15:53:30 +0100

    Added authentication.

--- Database.php
+++ Database.php
@@ -143,6 +143,30 @@ class Database
         return $whereClause;
     }
     
+    public function GetToken( $tokenId )
+    {
+        $where = new S\WhereEqual( 'id', $tokenId );
+        
+        return S\DB::RetrieveFirst(
+            $this->dbmsAdapter,
+            array( 'guid', 'level', 'accessed' ),
+            'tokens',
+            $where
+            );
+    }
+    
+    public function SaveTimestamp( $tokenId, $timestamp )
+    {
+        $where = new S\WhereEqual( 'id', $tokenId );
+        
+        S\DB::Update(
+            $this->dbmsAdapter,
+            'tokens',
+            array( 'accessed' => $timestamp ),
+            $where
+            );
+    }
+    
     private static function ExceptionHandler( PDOException $exception )
     {
         //SQLSTATE[42S02]: Base table or view not found: 1146 Table 'classicmodels.customers3' doesn't exist.
